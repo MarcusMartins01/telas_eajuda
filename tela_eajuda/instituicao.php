237be03049533c8c828e09e81c3d109a71b8ba6d
MarcusMartins01 <104526305+MarcusMartins01@users.noreply.github.com>
2022-11-14 15:29:44 -0300

    Add files via upload

---
+++ tela_eajuda/instituicao.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instituição</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav id="menu">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="voluntario.php">Voluntário</a></li>
+            <li><a href="instituicao.php">Instituição</a></li>
+            <li><a href="v_finan.php">Voluntário Financeiro</a></li>
+            <li><a href="cadastrar.php">Cadastre-se</a></li>
+            <li><a href="entrar.php">Entrar</a></li>
+        </ul>
+    </nav>
+
+    <p>Somente para indentificar a pagina, pode exluir</p>
+    <p>instituição</p>
+</body>
+</html>
